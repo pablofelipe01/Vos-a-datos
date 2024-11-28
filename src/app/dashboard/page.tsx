4f0e4fc78ce69997a1e9e5a9f3810cdbff0b0b29
14-David-06 <dhernandezg146@gmail.com>
2024-11-28 11:24:29 -0500

    Update page.tsx
    Añadi al dashboard el NavBar - David

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,15 @@
+import NavBar from '../components/NavBar'; // Asegúrate de ajustar la ruta según tu estructura de proyecto
+
 export default function dashboard() {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* Renderiza el NavBar */}
+      <NavBar />
+
+      {/* Contenido principal */}
+      <div className="flex items-center justify-center h-screen">
         <h1 className="text-3xl font-bold text-blue-600">Hola Mundo</h1>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
