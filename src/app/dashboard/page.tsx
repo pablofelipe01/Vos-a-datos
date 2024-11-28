ae79bd3de52875420ded6fe8dcc5dd2d9d4e2025
14-David-06 <dhernandezg146@gmail.com>
2024-11-28 08:50:50 -0500

    Update page.tsx
    Actualizacion de page de dashboar, "Hola Mundo" mientras se piensa en algo mas - David

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboard() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">Hola Mundo</h1>
+      </div>
+    );
+  }
+  
