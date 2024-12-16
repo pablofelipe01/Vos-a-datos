a14a69f585ac87f3bdbfac6b392b07f0ea474d20
14-David-06 <dhernandezg146@gmail.com>
2024-12-16 09:41:02 -0500

    Create page.tsx
    Agregando pestaña para que los operadores puedan seguir su turno - David

---
+++ src/app/seguimiento_turno/page.tsx
@@ -0,0 +1,32 @@
+import NavBar from '../components/NavBar'; // Asegúrate de ajustar la ruta según tu estructura de proyecto
+
+export default function dashboard() {
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat relative"
+      style={{ backgroundImage: "url('/h6.png')" }} // Usando la imagen como fondo
+    >
+      {/* Renderiza el NavBar */}
+      <NavBar />
+      <br />
+      <br />
+      <br />
+
+      {/* Contenido principal */}
+      <div className="flex flex-col items-center justify-center py-10">
+        {/* Airtable Embed */}
+        <iframe
+          className="airtable-embed"
+          src="https://airtable.com/embed/apprXBBomgiKhVc50/shrvKVJfv9zP65O9c"
+          frameBorder="0"
+          width="80%"
+          height="533"
+          style={{
+            background: "transparent",
+            border: "1px solid #ccc",
+          }}
+        ></iframe>
+      </div>
+    </div>
+  );
+}
